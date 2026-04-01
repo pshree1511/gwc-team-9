fbc70e707176b97dfc93d7e33199c3ed2b775a14
Brittany <brittanytn06@gmail.com>
2026-04-01 11:04:58 -0400

    fix background

--- point_lightMAIN.gd
+++ point_lightMAIN.gd
@@ -1,10 +1,15 @@
 extends PointLight2D
 
+var solved = false
+var rng = RandomNumberGenerator.new()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	off()
-	fade()
+	await fade()
+	await get_tree().create_timer(2).timeout
+	await blink()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,7 +18,22 @@ func _process(delta: float) -> void:
 func fade():
 	await get_tree().create_timer(2).timeout
 	var tween= get_tree().create_tween()
-	tween.tween_property(self, "energy", 1.3, 3)
+	tween.tween_property(self, "energy", 3, 3)
 
 func off():
 	self.energy=0.1
+
+func kindaoff(random):
+	var tween= get_tree().create_tween()
+	tween.tween_property(self, "energy", 2.5, random)
+	
+func blink():
+	rng.randomize()
+	self.energy=3
+	while !solved:
+		var random = rng.randf_range(0.5,1.0)
+		await get_tree().create_timer(1).timeout
+		var tween= get_tree().create_tween()
+		tween.tween_property(self, "energy", 3, random)
+		await get_tree().create_timer(1).timeout
+		kindaoff(random)
