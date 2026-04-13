85918fe752bcde8ab5edf7b972b206279d3bcb55
bluecybertiger <rbalwalli2017@gmail.com>
2026-04-13 15:16:03 -0400

    changes to game scene

---
+++ puzzle.gd
@@ -0,0 +1,6 @@
+extends Sprite2D
+
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
--- sprite_2d.gd
+++ sprite_2d.gd
@@ -8,3 +8,19 @@ func _on_texture_button_pressed() -> void:
 
 func _on_texture_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://boss.tscn")
+
+
+func _on_texture_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://puzzle.tscn")
+
+
+func _on_texture_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://butterfly.tscn")
+
+
+func _on_texture_button_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://boss.tscn")
+
+
+func _on_texture_button_6_pressed() -> void:
+	get_tree().change_scene_to_file("res://puzzle.tscn")
