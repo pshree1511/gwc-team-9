604f261919ba48aa4257242cfb687e20eb03ee10
bluecybertiger <rbalwalli2017@gmail.com>
2026-04-14 22:12:27 -0400

    fixed brittany's game back to original

--- button.gd
+++ button.gd
@@ -9,8 +9,6 @@ signal text_edited(solved)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Button.pressed.connect(change)
-	
-	%Button.visible = Globals.solved
 
 
 func change():
@@ -25,11 +23,8 @@ func _on_line_edit_2_text_submitted(new_text: String) -> void:
 	password = new_text
 	print(password)
 	if(password==key):
-		Globals.solved = true
-		
-		%Button.show()
-		
-		print(Globals.solved)
+		solved = true
+		print(solved)
 		Line.clear()
 		change()
 		%Button.hide()
